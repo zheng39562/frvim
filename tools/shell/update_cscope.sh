a5be4d19e3923222081948ac8e1732756288f040
zheng39562 <zheng39562@163.com>
2017-09-30 17:20:49 +0800

    * cscope add option : q

--- tools/shell/update_cscope.sh
+++ tools/shell/update_cscope.sh
@@ -10,8 +10,10 @@ echo " |- out_directory : ${frvim_out_directory}"
 
 echo " ... building"
 echo "cscope -i ${frvim_source_file} -kb -f _cscope.out"
-cscope -i ${frvim_source_file} -kb -f _cscope.out
+cscope -i ${frvim_source_file} -kbq -f _cscope.out
 mv _cscope.out ${frvim_out_directory}/cscope.out
+mv _cscope.out.in ${frvim_out_directory}/cscope.out.in
+mv _cscope.out.po ${frvim_out_directory}/cscope.out.po
 
 echo "=== End Build cscope ==="
 
